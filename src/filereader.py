6bddd00f0be27a70dbaf3850b2c32ea67fbdd364
Arun prasath <arunprasath33@gmail.com>
2018-05-05 13:19:10 +0530

    Added new functionalities to Filereader

--- src/filereader.py
+++ src/filereader.py
@@ -7,21 +7,6 @@ import os
 import glob
 import re
 
-# series = read_csv('usage/usage-2018-05-10.csv',
-#             names=['date','folder','size'],
-#             header=0,
-#             index_col=0,
-#             parse_dates=[0])
-# #print series.tail(10)
-# #print(series['2018-05-10 23-45'])
-# df = DataFrame(series)
-# #print df.loc['2018-05-10 23:40:00':'2018-05-10 23:50:00']
-# df_recent = df.loc[datetime.datetime(2018, 5, 10,23,40,0):datetime.datetime(2018, 5, 10,23,50,0)]
-# print df_recent
-# total=int(df_recent.loc[df_recent['folder'] == '/log','size'].item())
-# df_recent['percentage'] = (df_recent['size']/total)*100
-# print df_recent
-
 class CSVReader:
     def __init__(self, usage_location):
         self.path = usage_location
@@ -33,34 +18,49 @@ class CSVReader:
         series = None
         for f in all_files:
             file_date_match = re.search(r'\d{4}-\d{2}-\d{2}', f)
-            file_date = datetime.datetime.strptime(file_date_match.group(), '%Y-%m-%d').date()
-            if (file_date >= start_date and file_date <= end_date):
-                if 'gz' in f:
-                    series = read_csv(f,
-                                      names=['date','folder', 'size'],
-                                      header=0,
-                                      index_col=0,
-                                      parse_dates=[0],
-                                      compression='gzip'
-                                      )
-                else:
-                    series = read_csv(f,
-                                      names=['date','folder', 'size'],
-                                      header=0,
-                                      index_col=0,
-                                      parse_dates=[0],
-                                      )
-                list_.append(series)
+            if file_date_match:
+                file_date = datetime.datetime.strptime(file_date_match.group(), '%Y-%m-%d').date()
+                if (file_date >= start_date and file_date <= end_date):
+                    if 'gz' in f:
+                        series = read_csv(f,
+                                        names=['date','folder', 'size'],
+                                        header=0,
+                                        index_col=0,
+                                        parse_dates=[0],
+                                        compression='gzip'
+                                        )
+                    else:
+                        print f
+                        series = read_csv(f,
+                                        names=['date','folder', 'size'],
+                                        header=0,
+                                        index_col=0,
+                                        parse_dates=[0],
+                                        )
+                    list_.append(series)
         df = pandas.concat(list_)
         return df.sort_index()
 
-    def getLatest():
-        pass
+    def getLatest(self):
+        start_date = end_date = datetime.date.today()
+        return self.getBetweenDates(start_date, end_date)
 
+    def getLastNDays(self,num_days):
+        end_date = datetime.date.today()
+        start_date = end_date - datetime.timedelta(days=num_days)
+        return self.getBetweenDates(start_date, end_date)
 
 
 if __name__ == "__main__":
-    start_date=datetime.datetime.strptime('2018-05-06', '%Y-%m-%d').date()
-    end_date=datetime.datetime.strptime('2018-05-07', '%Y-%m-%d').date()
-    reader = CSVReader('/home/arun/Projects/bingoarun/sizemonitor/usage')
-    print reader.getBetweenDates(start_date,end_date)
+    reader = CSVReader('/home/arun/Projects/bingoarun/folmon/sample-data')
+
+    # Test getBetweenDates
+    #start_date=datetime.datetime.strptime('2018-05-1', '%Y-%m-%d').date()
+    #end_date=datetime.datetime.strptime('2018-05-10', '%Y-%m-%d').date()
+    #print reader.getBetweenDates(start_date,end_date)
+
+    # Test getLatest()
+    #print reader.getLatest()
+
+    #Test getLastNDays
+    print reader.getLastNDays(3)
