0f939a93429014d28f59256a909f38932a6bf013
Arun prasath <arunprasath33@gmail.com>
2018-05-07 18:19:16 +0530

    Added todo

--- command/cli.py
+++ command/cli.py
@@ -1,20 +1,28 @@
-import click
-from presentation import Presentation
+'''
+TODO commands
 
-presentation_obj = Presentation('/home/arun/Projects/bingoarun/folmon/sample-data')
+folmon status
+(show current overall status)
 
-@click.group()
-def main():
-    pass
+- to show diff in size, percentage increased for all below
 
-@click.command()
+folmon diff --start-date 14-01-2018 --end-date 20-01-2018 /usage/fruits
+(show the diff in size of all the folders under /usage/fruits also the overall folder size)
+  - to show diff in size, percentage increased
+
+folmon diff --numofdays 7 /usage/vegetables
+(show the diff in size of the folders under /usage/vegetables for the last 7 days)
+
+folmon show --numofdays 10 /usage
+(show a matrix of the folder and subfolder's size for the past 10 days )
+
+'''
 def status():
     presentation_obj.getRecentStatus()
+
+
     
 main.add_command(status)
 
 if __name__ == "__main__":
     main()
-
-
-
